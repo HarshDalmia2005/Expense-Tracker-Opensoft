04bb96396c18f5573a0c7db004934144e2ce04d8
Deploy Script <deploy@spends ense.local>
2026-08-15 13:56:03 +0000

    fix: correct mongoose connection syntax

--- backend/src/db/index.js
+++ backend/src/db/index.js
@@ -8,7 +8,7 @@ const connectDB = async()=>{
     try {
         await mongoose
         .connect(mongoDBURL)
-        ("connected to database of MongoDB")
+        .then(() => console.log("connected to database of MongoDB"))
         
     } catch (error) {
         console.log({message :error.message})
